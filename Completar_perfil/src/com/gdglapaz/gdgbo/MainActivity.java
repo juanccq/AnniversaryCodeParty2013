352cc01e0be7d2ff630d869a0d321fee11e5ed58
JuanSony <juan.c.c.q@gmail.com>
2013-05-17 23:49:52 -0400

    borrando archivos innecesarios

--- Completar_perfil/src/com/gdglapaz/gdgbo/MainActivity.java
+++ Completar_perfil/src/com/gdglapaz/gdgbo/MainActivity.java
@@ -2,16 +2,11 @@ package com.gdglapaz.gdgbo;
 
 
 
-import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.app.Activity;
-import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.view.Menu;
-import android.view.View;
 import android.widget.Button;
-import com.gdglapaz.gdgbo.store.SharedPreferencesCredentialStore;
-import com.gdglapaz.gdgbo.OAuthActivity;
 
 public class MainActivity extends Activity {
 	
@@ -23,7 +18,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		setup();
+
 	}
 
 	@Override
@@ -33,22 +28,5 @@ public class MainActivity extends Activity {
 		return true;
 	}
 	
-	private void setup() {
-		setContentView(R.layout.activity_main);
-
-		this.preferences = PreferenceManager.getDefaultSharedPreferences(this);
-
-		launchOauth = (Button) findViewById(R.id.btn_launch_oauth);
 	
-		launchOauth.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				startActivity(new Intent().setClass(v.getContext(),
-						OAuthActivity.class));
-			}
-		});
-
-			
-	
-	}
-
 }
